01d2350c511bd78efa68c2353e0dbde48585d80c
Dmytro <ihnatenko.dev@gmail.com>
2023-01-08 18:28:47 +0200

    Impl POST register and login

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import config from 'config';
-import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
+import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 export class App {
 	constructor() {
@@ -12,16 +14,75 @@ export class App {
 	}
 
 	useRoute() {
-		this.app.post('/auth/register', registerValidation, (req, res) => {
-			const errors = validationResult(req);
+		this.app.post('/auth/login', async (req, res) => {
+			try {
+				const { email, password } = req.body;
+				const user = await UserModel.findOne({ email });
 
-			if (!errors.isEmpty()) {
-				return res.status(400).json(errors.array());
+				if (!user) {
+					return res.status(404).json({ message: 'User not found' });
+				}
+
+				const checkPassword = await bcrypt.compare(password, user.passwordHash);
+				if (!checkPassword) {
+					return res.status(400).json({ message: 'Wrong login or password' });
+				}
+
+				const token = jwt.sign(
+					{
+						_id: user._id,
+					},
+					config.get('SECRET_KEY'),
+					{
+						expiresIn: '30d',
+					}
+				);
+
+				const { passwordHash, ...userData } = user._doc;
+
+				res.json({ ...userData, token });
+			} catch (error) {
+				res.status(400).json(`Authorization error: ${error}`);
 			}
+		});
 
-			res.json({
-				success: true,
-			});
+		this.app.post('/auth/register', registerValidation, async (req, res) => {
+			try {
+				const errors = validationResult(req);
+
+				if (!errors.isEmpty()) {
+					return res.status(400).json(errors.array());
+				}
+
+				const password = req.body.password;
+				const salt = await bcrypt.genSalt(10);
+				const hash = await bcrypt.hash(password, salt);
+
+				const newUser = new UserModel({
+					email: req.body.email,
+					fullName: req.body.fullName,
+					avatarUrl: req.body.avatarUrl,
+					passwordHash: hash,
+				});
+
+				const user = await newUser.save();
+
+				const token = jwt.sign(
+					{
+						_id: user._id,
+					},
+					config.get('SECRET_KEY'),
+					{
+						expiresIn: '30d',
+					}
+				);
+
+				const { passwordHash, ...userData } = user._doc;
+
+				res.json({ ...userData, token });
+			} catch (error) {
+				res.status(400).send(`Registration error: ${error}`);
+			}
 		});
 	}
 
@@ -33,7 +94,7 @@ export class App {
 		this.useMiddleware();
 		this.useRoute();
 		mongoose.set('strictQuery', true);
-		await mongoose.connect(config.get('dbURL'));
+		await mongoose.connect(config.get('DB_URL'));
 		this.app.listen(this.port, () => {
 			`Server started ${this.port}`;
 		});
