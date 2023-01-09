78e40a7107014619b4021af71f3c2387dd95faec
Dmytro <ihnatenko.dev@gmail.com>
2023-01-09 21:28:53 +0200

    Create Article model

---
+++ server/src/models/Article.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const ArticleSchema = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+
+		title: {
+			type: String,
+			required: true,
+		},
+
+		text: {
+			type: String,
+			required: true,
+		},
+
+		viewCount: {
+			type: Number,
+			default: 0,
+		},
+
+		tags: {
+			type: Array,
+			default: [],
+		},
+
+		imageUrl: String,
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	}
+);
+
+export default mongoose.model('Article', ArticleSchema);
