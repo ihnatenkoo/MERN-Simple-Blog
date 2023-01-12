57ea75f3314612c0121766f664fa428a176b053d
Dmytro <ihnatenko.dev@gmail.com>
2023-01-12 15:52:37 +0200

    Add react-router-dom, set routes

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
-
-import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
+import styles from './Header.module.scss';
 
 export const Header = () => {
 	const isAuth = false;
@@ -13,15 +13,15 @@ export const Header = () => {
 		<div className={styles.root}>
 			<Container maxWidth="lg">
 				<div className={styles.inner}>
-					<a className={styles.logo} href="/">
+					<Link className={styles.logo} to="/">
 						<div>MERN BLOG</div>
-					</a>
+					</Link>
 					<div className={styles.buttons}>
 						{isAuth ? (
 							<>
-								<a href="/posts/create">
+								<Link to="/posts/create">
 									<Button variant="contained">Написать статью</Button>
-								</a>
+								</Link>
 								<Button
 									onClick={onClickLogout}
 									variant="contained"
@@ -32,12 +32,12 @@ export const Header = () => {
 							</>
 						) : (
 							<>
-								<a href="/login">
+								<Link to="/login">
 									<Button variant="outlined">Войти</Button>
-								</a>
-								<a href="/register">
+								</Link>
+								<Link to="/register">
 									<Button variant="contained">Создать аккаунт</Button>
-								</a>
+								</Link>
 							</>
 						)}
 					</div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import CssBaseline from "@mui/material/CssBaseline";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App';
 
-import "./index.scss";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
+import './index.scss';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <CssBaseline />
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<CssBaseline />
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
+	</React.StrictMode>
 );
