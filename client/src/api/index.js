5571f348c697fbd9b5077e547e898ce5c4774934
Dmytro <ihnatenko.dev@gmail.com>
2023-01-21 10:58:29 +0200

    Set axios config - add Bearer to request

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const $api = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
 });
 
-export default instance;
+$api.interceptors.request.use((config) => {
+	if (config.headers) {
+		config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+	}
+	return config;
+});
+
+export default $api;
