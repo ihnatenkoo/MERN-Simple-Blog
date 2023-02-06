740bf1fa498451e0d654ca36d666868b8c9c1ff2
Dmytro <ihnatenko.dev@gmail.com>
2023-02-06 11:59:57 +0200

    Server: fix TagsService

--- server/src/tags/tags.service.js
+++ server/src/tags/tags.service.js
@@ -2,7 +2,7 @@ import ArticleModel from '../models/Article.js';
 
 export class TagsService {
 	async getLastTags() {
-		const articles = await ArticleModel.find().limit(5);
+		const articles = await ArticleModel.find().sort({ createdAt: -1 }).limit(5);
 		const tags = articles.map((a) => a.tags).flat();
 		const uniqTags = Array.from(new Set(tags)).splice(0, 5);
 		return uniqTags;
